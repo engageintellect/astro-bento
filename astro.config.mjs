f15e65ae27d8c7b136d022bb40b2e28fb5d14d62
engageintellect <jc9361@gmail.com>
2024-05-07 22:14:14 -0700

    adding vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,34 +4,26 @@ import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bento.engage-dev.com/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://bento.engage-dev.com/sitemap-index.xml",
-        "https://bento.engage-dev.com/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon(),
-    svelte(),
-  ],
+  integrations: [sitemap(), robotsTxt({
+    sitemap: ["https://bento.engage-dev.com/sitemap-index.xml", "https://bento.engage-dev.com/sitemap-0.xml"]
+  }), solidJs(), UnoCSS({
+    injectReset: true
+  }), icon(), svelte()],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
   output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
+  adapter: vercel(),
   vite: {
-    assetsInclude: "**/*.riv",
-  },
+    assetsInclude: "**/*.riv"
+  }
 });
